038d2b504bcba8147339bdaa5cb14ee9ce84400c
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:42:38 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// markenanmeldung-kosten - Markenanmeldung Kosten - Gebuehrenrechner fuer DPMA und EUIPO. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
